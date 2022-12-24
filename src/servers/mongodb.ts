c35ddea759297c4f5a7e8af540c9bd2cea4a0907
Victor <victor.martini@enacom.com.br>
2022-12-24 01:08:09 -0300

    finalizado

--- src/servers/mongodb.ts
+++ src/servers/mongodb.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 const connectionMongo = () => {
     mongoose.set("strictQuery", true);
     // mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@mongodb:27017/`);
-    mongoose.connect('mongodb+srv://victormdsp:81005496@cluster0.zm0nvyf.mongodb.net/produtos')
+    mongoose.connect(`mongodb://mongodb:27017/produtos`)
 }
 
 export default connectionMongo;
--- src/servers/postgres.ts
+++ src/servers/postgres.ts
@@ -2,20 +2,23 @@ require('dotenv').config;
 
 const pg = require('pg');
 
-// const pgConnection = new pg.Pool({
-//     user: 'postgres',
-//     host: 'postgres',
-//     database: process.env.POSTGRES_DB,
-//     password: process.env.POSTGRES_PASSWORD,
-//     port: 5432
-// })
-
 const pgConnection = new pg.Pool({
     user: 'postgres',
-    host: 'localhost',
-    database: 'Inovia',
-    password: '81005496',
-    port: 5432
+    host: 'postgres',
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
 })
+
+const connectToDb = async () => {
+    try {
+        await pgConnection.connect();
+    } catch (err) {
+        console.log(err);
+        console.log("EU SOU UM MACACACO");
+    }
+}
+
+connectToDb();
 
 export default pgConnection;
